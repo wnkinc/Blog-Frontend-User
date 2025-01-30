70a0e204d85b8cea92661d31adeeaa3ebb55a7cc
wnkinc <140204064+wnkinc@users.noreply.github.com>
2025-01-29 18:58:21 -0800

    Add basic create.ejs structure

--- src/controllers/create.controller.js
+++ src/controllers/create.controller.js
@@ -1,10 +1,12 @@
 // controllers/dashboard.controller.js
 const axios = require("axios");
+require("dotenv").config();
 
 // Function to load the dashboard page
 async function loadCreate(req, res, next) {
   res.render("create", {
     pageTitle: "Create Post",
+    tinyMCEApiKey: process.env.TINYMCE_API_KEY,
   });
 }
 
